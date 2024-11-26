3fee39e18f82af344ce034d25c97688b635b5e12
charan1435 <sc16101@gmail.com>
2024-11-26 21:51:40 +0530

    Initial Commit

---
+++ MockTestRecheck/MockTestRecheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockTestRecheckApp.swift
+//  MockTestRecheck
+//
+//  Created by Sabith Salman on 2024-11-26.
+//
+
+import SwiftUI
+
+@main
+struct MockTestRecheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MockTestRecheckTests/MockTestRecheckTests.swift
@@ -0,0 +1,17 @@
+//
+//  MockTestRecheckTests.swift
+//  MockTestRecheckTests
+//
+//  Created by Sabith Salman on 2024-11-26.
+//
+
+import Testing
+@testable import MockTestRecheck
+
+struct MockTestRecheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
